c4785e3b74293f9cbc9d3cec6046a2e3ded1292b
Felipe18pro <felipedacosta2901@gmail.com>
2024-10-17 14:12:50 -0300

    Mongoolllllllll

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const connectDB = async () => {
+    mongoose
+        .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`) 
+        .then(() => console.log('Connected to MongoDB')) 
+        .catch((error) => console.log(error)); 
+};
+
+export default connectDB;
